4956681492325a57aafab90ac02bc8d1233e6c54
Jerry <Jerry_Shen0527@outlook.com>
2020-10-17 17:30:14 +0800

    Fitter

---
+++ homeworks/project/src/hw1/Systems/Fitter.cpp
@@ -0,0 +1,32 @@
+﻿#include "Fitter.h"
+
+void PolynomialFitter::fit_data()
+{
+	using namespace std;
+	if (data.empty())
+	{
+		return;
+	}
+
+	vector<double> diff_quotion;
+
+	double temp = 0;
+	for (int i = 0; i < data.size(); ++i)
+	{
+		temp = y(i);
+		for (int j = 0; j < i; ++j)
+		{
+			diff_quotion[j] = temp;
+			temp = diff_quotion[j];
+
+			diff_quotion[j] = temp;
+		}
+		diff_quotion.push_back(temp);
+	}
+
+	rst =
+		[&diff_quotion, this](double)->double
+	{
+		return  0;
+	};
+}
